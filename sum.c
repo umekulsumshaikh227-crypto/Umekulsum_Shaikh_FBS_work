e711ae41c6abef4e863a3043b22466059d1733ac
umekulsumshaikh227-crypto <umekulsumshaikh227@gmail.com>
2026-07-24 10:26:29 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b, sum;
+	
+	
+	printf("enter first number:");
+	
+	scanf("%d", &a);
+	
+	printf("Enter second number:");
+	
+	scanf("%d", &b);
+	
+	sum= a + b;
+    
+    printf("sum = %d", sum);
+    
+}
