b489a893083cd00bf938bd7acc9c98b6f7853edd
umekulsumshaikh227-crypto <umekulsumshaikh227@gmail.com>
2026-07-28 10:22:24 +0530

    Add files via upload

---
+++ square and cube.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num, square, cube;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    square = num * num;
+    cube = num * num * num;
+
+    printf("Square = %d\n", square);
+    printf("Cube = %d\n", cube);
+
+    return 0;
+}
